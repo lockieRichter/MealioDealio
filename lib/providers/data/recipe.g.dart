7cfa40e6df713791fad3e4b0feaa34190f4fb2b0
Lockie Richter <richter.lockie@gmail.com>
2023-03-10 23:16:57 +1030

    Implement recipe providers and page

---
+++ lib/providers/data/recipe.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recipe.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$recipeHash() => r'f605d8783a25cc2199faca520dcb2a6bc4192c11';
+
+/// See also [Recipe].
+@ProviderFor(Recipe)
+final recipeProvider =
+    AutoDisposeAsyncNotifierProvider<Recipe, List<String>>.internal(
+  Recipe.new,
+  name: r'recipeProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$recipeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Recipe = AutoDisposeAsyncNotifier<List<String>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
